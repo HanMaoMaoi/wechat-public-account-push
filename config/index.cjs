2bc9cf16e8d8ad05b4858947f5f8e98057a27a54
HanMaoMaoi <112368692+HanMaoMaoi@users.noreply.github.com>
2023-06-30 20:44:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,18 +8,18 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx4bbd837570acde14',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '257a26cddb0d16a7b5d6617afee4968e',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '浙江',
+  CITY: '上虞',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '小兔子',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
